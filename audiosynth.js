b80b65998c361f27ef8e5c5af57a398cee97ab36
Robocittykat <115361685+Robocittykat@users.noreply.github.com>
2025-03-16 20:55:35 -0400

    fully implemented the major chords. Some tweaking may still be needed. Also set up the framework so that future chord sets should be easy

--- audiosynth.js
+++ audiosynth.js
@@ -91,12 +91,13 @@ var Synth, AudioSynth, AudioSynthInstrument;
 		octave |= 0;
 		octave = Math.min(8, Math.max(1, octave));
 		var time = !duration?2:parseFloat(duration);
-		if(typeof(this._notes[note])=='undefined') { throw new Error(note + ' is not a valid note.'); }
-		if(typeof(this._fileCache[sound][octave-1][note][time])!='undefined') {
-			if(this._debug) { console.log((new Date).valueOf() - t, 'ms to retrieve (cached)'); }
-			return this._fileCache[sound][octave-1][note][time];
+		//if(typeof(this._notes[note])=='undefined') { throw new Error(note + ' is not a valid note.'); }
+		//if(typeof(this._fileCache[sound][octave-1][note][time])!='undefined') {
+		//	if(this._debug) { console.log((new Date).valueOf() - t, 'ms to retrieve (cached)'); }
+		//	return this._fileCache[sound][octave-1][note][time];
+		if(false){ //this shouldn't be here unless the above is commented out
 		} else {
-			var frequency = this._notes[note] * Math.pow(2,octave-4);
+			var frequency = note * Math.pow(2,octave-4);
 			var sampleRate = this._sampleRate;
 			var volume = this._volume;
 			var channels = this._channels;
@@ -150,7 +151,7 @@ var Synth, AudioSynth, AudioSynthInstrument;
 			];
 			var blob = new Blob(out, {type: 'audio/wav'});
 			var dataURI = URL.createObjectURL(blob);
-			this._fileCache[sound][octave-1][note][time] = dataURI;
+			this._fileCache[sound][octave-1]["C#"][time] = dataURI;
 			if(this._debug) { console.log((new Date).valueOf() - t, 'ms to generate'); }
 			return dataURI;
 		}
--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 let inst = 0
+let hs = 2**(1/12)
+let ws = 2**(1/6)
+let letters = {'C':261.63,'C#':277.18,'D':293.66,'D#':311.13,'E':329.63,'F':349.23,'F#':369.99,'G':392.00,'G#':415.30,'A':440.00,'A#':466.16,'B':493.88}
 
-function playChord(n1,n2,n3){
-    Synth.play(inst,n1,4,4)
-    Synth.play(inst,n2,4,4)
-    Synth.play(inst,n3,4,4)
+function playChord(startFreq,type,octave){
+    if(typeof startFreq == 'string'){
+		startFreq = letters[startFreq]
+	}
+	
+	startFreq *= 2**(octave-4)
+	
+	notes = [startFreq,]
+	
+	switch(type){
+		case "maj":
+			notes.push(startFreq*hs**4)
+			notes.push(startFreq*hs**7)
+	}
+	
+	for(i of notes){
+		Synth.play(inst,i,4,1)
+	}
+	
 }
+
+
