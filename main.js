b769f3d8290f4957474d4267f2f1a0a1eda57a06
Robocittykat <115361685+Robocittykat@users.noreply.github.com>
2025-03-16 21:29:01 -0400

    added all chords!!!

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let inst = 0
+let inst = 2
 let hs = 2**(1/12)
 let ws = 2**(1/6)
 let letters = {'C':261.63,'C#':277.18,'D':293.66,'D#':311.13,'E':329.63,'F':349.23,'F#':369.99,'G':392.00,'G#':415.30,'A':440.00,'A#':466.16,'B':493.88}
@@ -8,7 +8,7 @@ function playChord(startFreq,type,octave){
 		startFreq = letters[startFreq]
 	}
 	
-	startFreq *= 2**(octave-4)
+	startFreq *= 2**(octave-5)
 	
 	notes = [startFreq,]
 	
@@ -16,6 +16,35 @@ function playChord(startFreq,type,octave){
 		case "maj":
 			notes.push(startFreq*hs**4)
 			notes.push(startFreq*hs**7)
+			break
+		case "min":
+			notes.push(startFreq*hs**3)
+			notes.push(startFreq*hs**7)
+			break
+		case "7":
+			notes.push(startFreq*hs**4)
+			notes.push(startFreq*hs**7)
+			notes.push(startFreq*hs**10)
+			break
+		case "M7":
+			notes.push(startFreq*hs**4)
+			notes.push(startFreq*hs**7)
+			notes.push(startFreq*hs**11)
+			break
+		case "m7":
+			notes.push(startFreq*hs**3)
+			notes.push(startFreq*hs**7)
+			notes.push(startFreq*hs**10)
+			break
+		case "dim":
+			notes.push(startFreq*hs**3)
+			notes.push(startFreq*hs**6)
+			break
+		case "sus":
+			notes.push(startFreq*hs**5)
+			notes.push(startFreq*hs**7)
+			break
+		
 	}
 	
 	for(i of notes){
