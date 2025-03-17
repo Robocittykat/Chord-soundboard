5da12f29e20638b4171ded0d66ff8a6350ceecb5
Robocittykat <115361685+Robocittykat@users.noreply.github.com>
2025-03-17 08:26:58 -0400

    tweaked some appearance things, added octave

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ let ws = 2**(1/6)
 let letters = {'C':261.63,'C#':277.18,'D':293.66,'D#':311.13,'E':329.63,'F':349.23,'F#':369.99,'G':392.00,'G#':415.30,'A':440.00,'A#':466.16,'B':493.88}
 
 function playChord(startFreq,type,octave){
-    if(typeof startFreq == 'string'){
+    octave = octaveSlide.value
+	if(typeof startFreq == 'string'){
 		startFreq = letters[startFreq]
 	}
 	
-	startFreq *= 2**(octave-5)
+	startFreq *= 2**(octave-4)
 	
 	notes = [startFreq,]
 	
@@ -53,4 +54,6 @@ function playChord(startFreq,type,octave){
 	
 }
 
-
+function update(){
+	octave.innerHTML = octaveSlide.value
+}setInterval(update,0)
